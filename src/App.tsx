f8fbf691c239211508dc1b2a9976189133116726
recep-demir <demir.rp@gmail.com>
2025-02-24 18:59:46 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
   
 
   return (
     <>
-      
+      <Home/>
     </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,4 @@
-import React from 'react'
-
-type Props = {}
-
-function Home({}: Props) {
+const Home = () => {
   return (
     <div>Home</div>
   )
