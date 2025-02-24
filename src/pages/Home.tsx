bef0cb3955a1c4afe17ae7c5d028aedeb651d327
recep-demir <demir.rp@gmail.com>
2025-02-24 19:54:57 +0100

    add interface in Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,32 @@
+import axios from "axios"
 import AddTodo from "../components/AddTodo"
 import Header from "../components/Header"
 import TodoList from "../components/TodoList"
+import { useEffect, useState } from "react"
+
+
 
 const Home = () => {
+const url= "https://67bcb04bed4861e07b3b91af.mockapi.io/task/task/"
 
-    const getTodo = ()=>{
-        
+interface ITodo{
+    id:string;
+    isDone:boolean;
+    task:string;
+}
+
+const[todos,setTodos]=useState<ITodo[]>([])
+
+const getTodo = async ()=>{
+        const {data} =await axios(url)
+        console.log(data)
+        setTodos(data)
     }
+
+useEffect(()=>{
+    getTodo()
+    },[])
+
   return (
     <div>
         <Header/>
