85441ab12c08916f58ae67b80eab8e2352bb1c2f
Recep Demir <recep.demir@powercoders.org>
2026-03-14 15:19:49 +0100

    refactor: enhance AddTodo component structure and improve form handling

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,57 +1,76 @@
 import { Box, Button, Paper, TextField } from "@mui/material";
 import { useState } from "react";
 
-interface IAddTodo {
-  addTodo: AddFn;
+interface IAddTodo{
+    addTodo:AddFn
 }
 
-const AddTodo: React.FC<IAddTodo> = ({ addTodo }) => {
+const AddTodo:React.FC<IAddTodo> = ({addTodo}) => {
   const [task, setTask] = useState("");
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setTask(e.target.value);
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!task.trim()) return;
-    addTodo(task.trim());
-    setTask("");
+    addTodo(task)
+    setTask("")
   };
 
   return (
     <Paper
-      component="form"
-      onSubmit={handleSubmit}
       elevation={3}
       sx={{
         p: 2,
-        display: "flex",
-        gap: 1,
-        alignItems: "center",
-        mb: 2,
+        borderRadius: 2,
+        maxWidth: 600,
+        mx: "auto",
+        mt: 3,
+        background: "linear-gradient(90deg, #7310ec, #ff2bbf)",
       }}
     >
-      <TextField
-        value={task}
-        onChange={handleChange}
-        placeholder="Enter a task..."
-        variant="outlined"
-        fullWidth
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
         sx={{
-          flex: 1,
-          "& .MuiOutlinedInput-root": {
-            borderRadius: 2,
-            backgroundColor: "#f3e5f5",
-          },
+          display: "flex",
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: "center",
+          gap: 2,
+          
         }}
-      />
-      <Box>
-        <Button type="submit" variant="contained" color="secondary">
-          SAVE
-        </Button>
-      </Box>
-    </Paper>
+      >
+        <TextField
+      value={task}
+      //   onChange={(e)=>setTask(e.target.value)}
+      onChange={handleChange}
+      placeholder="Enter a task..."
+      variant="outlined"
+      fullWidth
+      sx={{
+        flex: 1,
+      "& .MuiOutlinedInput-root": {
+          borderRadius: 2,
+          backgroundColor: "#e1bee7",
+        },
+      }}
+    />
+    <Button  type="submit"
+      variant="contained"
+      
+      sx={{
+        px: 3,
+        py: 1.5,
+        borderRadius: 2,
+        fontWeight: "bold",
+        textTransform: "none",
+        background: "linear-gradient(90deg, #7310ec, #ff2bd1)"
+      }}>SAVE</Button>
+  </Box>
+</Paper>
   );
 };
 
