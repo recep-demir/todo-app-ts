3c61174b95668912ce49eea190cef42551303f48
Recep Demir <recep.demir@powercoders.org>
2026-03-14 14:24:32 +0100

    refactor: improve formatting and structure of ITodo interface and type definitions in typescript.d.ts

--- src/typescript.d.ts
+++ src/typescript.d.ts
@@ -1,10 +1,9 @@
-
-interface ITodo{
-    task:string,
-    isDone:boolean,
-    id:string
+interface ITodo {
+  id: string;
+  task: string;
+  isDone: boolean;
 }
 
-type AddFn=(task:string)=>Promise<void>
-type ToggleFn=(todo:ITodo)=>Promise<void>
-type DeleteFn=(id:string | number)=>Promise<void>
+type AddFn = (task: string) => Promise<void>;
+type ToggleFn = (todo: ITodo) => Promise<void>;
+type DeleteFn = (id: string | number) => Promise<void>;
