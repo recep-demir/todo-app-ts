d5267f7653da6eba6b6396af6e4f93ea70b19070
Recep Demir <recep.demir@powercoders.org>
2026-03-14 15:19:39 +0100

    refactor: remove commented-out CRUD operations in todo.router.js

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -1,29 +1,12 @@
 
 "use strict";
-/* -------------------------------------------------------
-    EXPRESSJS - TODO Project with Sequelize
-------------------------------------------------------- */
+
 
 const router = require("express").Router();
 
 const todo = require('../controllers/todo.controller');
 
-// // List
-// router.get('/todos', todo.list);
 
-// //* CRUD OPERATIONS
-
-// // Create
-// router.post('/todos', todo.create);
-
-// // Read
-// router.get('/todos/:id', todo.read);
-
-// // Update
-// router.put('/todos/:id', todo.update);
-
-// // Delete
-// router.delete('/todos/:id', todo.delete);
 
 
 router.route('/todos')
